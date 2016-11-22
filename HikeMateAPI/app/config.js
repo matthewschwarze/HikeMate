4e3070042275885f1cad7bdcace388e78325f304
matthew <mls997@uowmail.edu.au>
2016-11-22 15:21:23 +1100

    added basic node.js structure

---
+++ HikeMateAPI/app/config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+
+    'secret': 'ilovescotchyscotch',
+    'database': {
+		  user: 'HikeMateAdmin', //env var: PGUSER 
+		  database: 'HikeMate', //env var: PGDATABASE 
+		  password: 'PRESTRONG', //env var: PGPASSWORD 
+		  port: 5432, //env var: PGPORT 
+		  max: 10, // max number of clients in the pool 
+		  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+		}
+
+};
