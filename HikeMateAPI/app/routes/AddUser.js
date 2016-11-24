b7d43048faf1c17b98dbf84e37d06cb861aeead4
matthew <mls997@uowmail.edu.au>
2016-11-25 08:56:45 +1100

    added pword validation

--- HikeMateAPI/app/routes/AddUser.js
+++ HikeMateAPI/app/routes/AddUser.js
@@ -1,11 +1,35 @@
-var sha1 = require('sha1');
+const sha1 = require('sha1');
 const crypto = require('crypto');
+const validator = require('validator');
 
 module.exports = {
 	AddUser: function (req, res, next){
 	  // Grab data from http request
 	 console.log(req.body);
-	 const results = [];
+	 var results = [];
+	 
+	 //check if data is valid
+	if(req.body.UserName == "", req.body.Email == "", req.body.Password == ""){ //empty
+		return res.status(500).json({success: false, status: 500, data: {err: "One or more fields cannot be blank"}});
+	}
+	var username = req.body.UserName + '';
+	var email = req.body.Email + '';
+	var password = req.body.Password + '';
+	
+	if(/^[a-zA-Z0-9-_]*$/.test(username) == false) {
+	    return res.status(500).json({success: false, status: 500, data: {err: "Username can only contain numbers, letters, underscores and hyphens"}});
+	}
+	var regularExpression = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9%^*!@#$&()\\-_`.+,/\"]{6,}$/;
+	if(regularExpression.test(password) == false) {
+	    return res.status(500).json({success: false, status: 500, data: {err: "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number and 6 or more characters"}});
+	}
+	
+	
+	validator.normalizeEmail(email, [true]) 
+	if(!validator.isEmail(email)){
+		return res.status(500).json({success: false, status: 500, data: {err: "Invalid email format"}});
+	}
+	 
 	  // Get a Postgres client from the connection pool
 	  pool.connect((err, client, done) => {
 	    // Handle connection errors
@@ -16,18 +40,18 @@ module.exports = {
 	    }
 	    // SQL Query > Insert Data
 		
-	   var salt = crypto.randomBytes(256, (err, buf) => {
+	   var salt = crypto.randomBytes(256);
+	   /*crypto.randomBytes(256, (err, buf) => {
 		 	 if (err) throw err;
 		  		console.log(`${buf.length} bytes of random data: ${buf.toString('base64')}`);
 		  		return buf.toString('base64');
-		});
-		
+		}); */
 		var NewSalt = sha1(salt); //encode salt
-		var encodedpword =sha1(req.body.Password + NewSalt); //already sha1 pword with sha1 salt
+		var encodedpword =sha1(password + NewSalt); //un hashed pword with sha1 salt
 
 
 	   var query = client.query('INSERT INTO "Users" ("UserName", "Email", "Password", "Salt") values($1, $2, $3, $4)',
-	   	[req.body.UserName, req.body.Email, encodedpword, NewSalt], function(err, result){
+	   	[username, email, encodedpword, NewSalt], function(err, result){
 	   	
 			    if(err) {
 			    	done();
