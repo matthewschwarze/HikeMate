0fcb5399a1a366da8538870a7524267230d0f43a
matthew <mls997@uowmail.edu.au>
2016-11-25 19:33:41 +1100

    added token auth to protected routes

--- HikeMateAPI/app/routes.js
+++ HikeMateAPI/app/routes.js
@@ -7,7 +7,43 @@ var Useradd = require('./routes/AddUser');
 var Login = require('./routes/Login');
 var Test = require('./routes/Test');
 
-	router.route('/test').get(Test.Test); 
 	router.route('/AddUser').post(Useradd.AddUser); 
 	router.route('/Login').post(Login.Login); 
+	
+	
+	
+	router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+router.route('/test').get(Test.Test); 
+
+	
 module.exports = router;
